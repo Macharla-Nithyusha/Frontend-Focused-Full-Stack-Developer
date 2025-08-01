58907904f806d9a5c9db98a929e804cc133e7f6f
Macharla-Nithyusha <125576114+Macharla-Nithyusha@users.noreply.github.com>
2025-08-01 22:13:59 +0530

    Update App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,1 +1,15 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import LandingPage from './pages/LandingPage';
+import DoctorProfile from './pages/DoctorProfile';
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/doctor/:id" element={<DoctorProfile />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
 
